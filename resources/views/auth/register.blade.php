425473a8f98600e7836ce4406fb6630547b0fe0b
Levan <echolevan@gmail.com>
2017-09-09 22:11:09 +0800

    添加找回密码

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
-    <title>嘿市</title>
+    <title>嘿市 - 分享快乐 愉悦他人</title>
     <script>
         window.Laravel = {!! json_encode([
             'csrfToken' => csrf_token(),
