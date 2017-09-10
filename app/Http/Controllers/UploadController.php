640fa0a2678c27515d385bda3a6b9753e0da1080
iwowcn <iwowcn@gmail.com>
2017-09-10 14:25:24 +0800

    add

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -54,7 +54,7 @@ class UploadController extends Controller
     public function upload_plug_info_plug(Request $request)
     {
         if(!in_array(strtolower($request->file('file')->getClientOriginalExtension()) , config('my.upload_plug_type'))){
-            return ['sta'=> 0 ,'msg'=>'请上传zip、rar、7z格式的图片'];
+            return ['sta'=> 0 ,'msg'=>'请上传zip、rar、7z格式的文件'];
         }
 
         if($request->file('file')->getSize() > 1024*1024){
