9f4361ecbf4525be06dad7b0a1afca3334ed67bc
iwowcn <iwowcn@gmail.com>
2017-09-20 18:27:45 +0800

    修改描述

--- resources/assets/js/components/admin/charts/InfoCharts.vue
+++ resources/assets/js/components/admin/charts/InfoCharts.vue
@@ -3,8 +3,8 @@
     <div>
         <tbody v-if="info">
         <tr>
-            <td width="10%">服务器当前时间</td>
-            <td width="20%"><span id="stime">{{info.sysinfo.time}}</span></td>
+            <td width="20%">服务器当前时间</td>
+            <td width="32.51%"><span id="stime">{{info.sysinfo.time}}</span></td>
             <td width="20%">服务器运行时间</td>
             <td width="30%" colspan="3"><span id="uptime">{{info.sysinfo.uptime}}</span></td>
         </tr>
