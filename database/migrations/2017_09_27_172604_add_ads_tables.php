b0263beca5087cc7858c44d90cbfcf87c2bc6350
Levan <echolevan@gmail.com>
2017-09-28 15:21:42 +0800

    guanggao

--- database/migrations/2017_09_27_172604_add_ads_tables.php
+++ database/migrations/2017_09_27_172604_add_ads_tables.php
@@ -17,6 +17,7 @@ class AddAdsTables extends Migration
             $table->increments('id');
             $table->tinyInteger('position')->comment('位置');
             $table->string('url',300)->comment('图片地址');
+            $table->string('link')->comment('url');
             $table->string('width',10)->comment('图片显示宽度');
             $table->string('height',10)->comment('图片显示宽度');
             $table->tinyInteger('is_show')->default(1)->comment('是否显示 1显示 0不显示');
