b21d5c111771f4a1f7352361c3cfbfeb1a2b2d8e
Levan <echolevan@gmail.com>
2017-10-03 15:33:18 +0800

    ·

--- resources/assets/js/components/admin/Index.vue
+++ resources/assets/js/components/admin/Index.vue
@@ -85,21 +85,6 @@
                     <Menu-item name="admin.tag.create">添加分类</Menu-item>
                 </router-link>
             </Submenu>
-            <!--<Submenu name="3">-->
-                <!--<template slot="title">-->
-                    <!--<Icon type="stats-bars"></Icon>-->
-                    <!--统计分析-->
-                <!--</template>-->
-                <!--<Menu-group title="使用">-->
-                    <!--<Menu-item name="3-1">新增和启动</Menu-item>-->
-                    <!--<Menu-item name="3-2">活跃分析</Menu-item>-->
-                    <!--<Menu-item name="3-3">时段分析</Menu-item>-->
-                <!--</Menu-group>-->
-                <!--<Menu-group title="留存">-->
-                    <!--<Menu-item name="3-4">留存用户</Menu-item>-->
-                    <!--<Menu-item name="3-5">流失用户</Menu-item>-->
-                <!--</Menu-group>-->
-            <!--</Submenu>-->
         </Menu>
         <div class="main content">
             <div class="head_title">
